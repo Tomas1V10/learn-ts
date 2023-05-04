50dd1c3ba3ba3bc479d234f97c1003a757f3b4a6
Guille15032 <guilleps082020@gmail.com>
2023-05-03 21:30:02 -0500

    pilares, clases, encapsulamiento

--- src/basics/exercise-1/app.ts
+++ src/basics/exercise-1/app.ts
@@ -48,3 +48,75 @@ let exampleTuple: [string, null] = ['hola', null ];
 type statusCode = 'active' | 'inactive';
 
 let studenStatus: statusCode = 'inactive';
+
+// Any
+let working: any = 25;
+working = 'hola';
+
+
+//Funciones
+//function  nombre de la función(parámetro: tipoDato): tipoDeDatoQueRetornaLaFunción
+function validateValue( value: unknown ): boolean {
+
+    //unknown que es un tipo de dato que es desconocido
+    //para cualquier validación que daba hacer con el dato
+    //debo primero saber o validar que tipo de dato es 
+    if (typeof value === 'string') {
+        console.log(value.trim().toUpperCase());
+    }
+
+    if (value === '') {
+        return false;
+    } else {
+        return true;
+    }
+
+}
+
+function getFullName(firstName: string, lastName: string,): string {
+
+    return `${firstName} ${lastName}`;
+
+}
+
+//Void
+function processResponse( saved: boolean ) {
+    if (saved) {
+        console.log('Guardado');
+    } else {
+        console.log('Ocurrió un error');
+    }
+}
+
+
+console.log( getFullName(firstName, lastName) );
+processResponse(true);
+
+enum Role {
+    Admin,
+    Client,
+    Users,
+    Super  
+}
+
+let users: Array< {name: string, role: string } > = [
+
+    {
+        name: 'Pepito',
+        role: Role.Admin
+    },
+    {
+        name: 'Juanito',
+        role: Role.Client
+    },
+    {
+        name: 'Fulanito',
+        role:  Role.Client
+    }
+
+
+]
+
+console.log( users.filter( (user) => user.role === Role.Client ) );
+
+
