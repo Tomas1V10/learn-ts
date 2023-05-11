8c1a88ea953fb2aa0b33222e647a61954fe5e8b4
Tomas Vargas <vargastomas927@gmail.com>
2023-05-10 21:43:18 -0500

    Ejercisio de programamcion orientada a objetos

--- src/poo/Mujer.ts
+++ src/poo/Mujer.ts
@@ -1,16 +1,19 @@
 import { IPersona } from "./IPersona";
 
 export class Mujer implements IPersona {
-   public nombre!: string;
-   public apellido!: string;
-   public  edad!: number;
 
-   public nombre_completo(): string {
-       return `${this.nombre} ${this.apellido}`;
-   }
+    public edad!: number;
 
-   public ir_cine(sala: string, película: string): void {
+    constructor(public nombre: string, public apellido: string) {
+
+    }
+
+    public nombre_completo(): string {
+        return `${this.nombre} ${this.apellido}`;
+    }
+
+    public ir_cine(sala: string, película: string): void {
         console.log(`Viendo ${película} en ${sala}`);
-   }
+    }
 
 }
